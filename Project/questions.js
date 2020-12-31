8247657ae4c024b475ac3210309bbeff5316e792
Abdurrahman <abdurrahman.sapoglu@stu.fsm.edu.tr>
2020-12-31 13:02:31 +0300

    Proje update

---
+++ Project/questions.js
@@ -0,0 +1,104 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    question: "Kendinizi başka kişilere tanıtmayı zor buluyorsunuz",
+    options: [
+      "-2",
+      "-1",
+      "0",
+      "+1",
+      "+2"
+    ]
+  },
+    {
+    question: "Çoğunlukla düşüncelerinizde öyle kayboluyorsunuz ki, etrafınızdakileri görmezden geliyor veya unutuyorsunuz",
+    options: [
+      "-2",
+      "-1",
+      "0",
+      "+1",
+      "+2"
+    ]
+  },
+    {
+    question: "E-postalarınıza mümkün olduğu kadar çabuk yanıt vermeye çalışıyor ve dağınık bir gelen kutusuna tahammül edemiyorsunuz",
+    options: [
+      "-2",
+      "-1",
+      "0",
+      "+1",
+      "+2"
+    ]
+  },
+    {
+    question: "Biraz baskı olan durumlarda bile, kolay şekilde rahat ve odaklı kalabiliyorsunuz",
+    options: [
+      "-2",
+      "-1",
+      "0",
+      "+1",
+      "+2"
+    ]
+  },
+    {
+    question: "Genellikle konuşmaları başlatan taraf siz değilsiniz",
+    options: [
+      "-2",
+      "-1",
+      "0",
+      "+1",
+      "+2"
+    ]
+  },
+  {
+    question: "Nadiren sırf meraktan bir şey yapıyorsunuz",
+    options: [
+      "-2",
+      "-1",
+      "0",
+      "+1",
+      "+2"
+    ]
+  },
+  {
+    question: "Kendinizi diğer insanlardan üstün hissediyorsunuz",
+    options: [
+      "-2",
+      "-1",
+      "0",
+      "+1",
+      "+2"
+    ]
+  },
+  {
+    question: "Sizin için düzenli olmak uyum sağlamaktan daha önemli",
+    options: [
+      "-2",
+      "-1",
+      "0",
+      "+1",
+      "+2"
+    ]
+  },
+  {
+    question: "Genellikle yüksek motivasyona sahip ve enerjiksiniz",
+    options: [
+      "-2",
+      "-1",
+      "0",
+      "+1",
+      "+2"
+    ]
+  },
+  {
+    question: "Bir tartışmayı kazanmak, kimsenin üzülmemesini sağlamaktan daha önemsiz",
+    options: [
+      "-2",
+      "-1",
+      "0",
+      "+1",
+      "+2"
+    ]
+  },
+
+];
